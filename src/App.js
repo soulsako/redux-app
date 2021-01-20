73bcbeae783c6498fabaaf9111507287bcee71b2
Mohammad <hamid.mian20@gmail.com>
2021-01-20 11:17:53 +0000

    added mui theme

--- src/App.js
+++ src/App.js
@@ -1,70 +1,16 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from 'react';
 import './App.css';
-import { actionGetAllUsers } from './actions/actionUser';
-import { getAllUsers, getUsersLoading, getUsersError, getUsersErrorMessage } from './reducers/reducerUser';
-import { ModelUser } from './models';
 
-function App({
-  objModelUsers,
-  bIsLoading,
-  bIsError,
-  fetchAllUsers,
-  strErrorMessage,
-}) {
-  useEffect(() => {
-    fetchAllUsers();
-  },[]);
 
-  if (bIsLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (!bIsLoading && bIsError) {
-    return <p>{strErrorMessage}</p>
-  }
-
-  console.log("objModelUsers", objModelUsers);
+function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-            {objModelUsers && Object.values(objModelUsers).map(objUser => (
-              <ul key={objUser.getField(ModelUser.FIELD_STR_ID)}>
-                <li>{objUser.getField(ModelUser.FIELD_STR_FIRST_NAME)}</li>
-                <li>{objUser.getField(ModelUser.FIELD_STR_LAST_NAME)}</li>
-                <li>{objUser.getField(ModelUser.FIELD_STR_EMAIL)}</li>
-                <li>{objUser.getField(ModelUser.FIELD_STR_AVATAR)}</li>
-              </ul>
-            ))}
-        </div>
+       Hello world
       </header>
     </div>
   );
 }
 
-App.defaultProps = {
-  objModelUsers: null,
-};
-
-App.propTypes = {
-  objModelUsers: PropTypes.arrayOf(
-    PropTypes.instanceOf(ModelUser),
-  ),
-  fetchAllUsers: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => ({
-  objModelUsers: getAllUsers(state),
-  bIsLoading: getUsersLoading(state),
-  bIsError: getUsersError(state),
-  strErrorMessage: getUsersErrorMessage(state)
-});
-
-const mapDispatchToProps = {
-  fetchAllUsers: actionGetAllUsers 
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
+import theme from './theme';
 
 ReactDOM.render(
-  <Provider store={store()}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store()}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </MuiThemeProvider>
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 
